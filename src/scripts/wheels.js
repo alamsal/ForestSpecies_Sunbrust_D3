4e61c389578507dbbaf34f2d37c0ae7ea1976cc8
Ashis <lamsalwork@gmail.com>
2015-08-06 10:46:51 -0600

    Wheel with label names

--- src/scripts/wheels.js
+++ src/scripts/wheels.js
@@ -13,7 +13,7 @@ var color = d3.scale.category20c();
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
 
 var partition = d3.layout.partition()
@@ -31,31 +31,52 @@ var node;
 
 d3.json("data/forestSpecies.json", function(error, root) {
   node = root;
-  var path = svg.datum(root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .on("click", click)
-      .each(stash);
+  var g = svg.selectAll("g")
+      .data(partition.nodes(root))
+    .enter().append("g");
+
+  var path = g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+    .on("click", click);
+
+  var text = g.append("text")
+    .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+    .attr("x", function(d) { return y(d.y); })
+    .attr("dx", "6") // margin
+    .attr("dy", ".35em") // vertical-align
+    .text(function(d) { return d.name; });
+
 
   d3.selectAll("input").on("change", function change() {
     var value = this.value === "count"
         ? function() { return 1; }
         : function(d) { return d.size; };
 
-    path
-        .data(partition.value(value).nodes)
-      .transition()
+    path.data(partition.value(value).nodes)
+        .transition()
         .duration(1000)
         .attrTween("d", arcTweenData);
   });
 
   function click(d) {
-    node = d;
+    // fade out all text elements
+    text.transition().attr("opacity", 0);
     path.transition()
-      .duration(1000)
-      .attrTween("d", arcTweenZoom(d));
+      .duration(750)
+      .attrTween("d", arcTween(d))
+      .each("end", function(e, i) {
+          // check if the animated element's data e lies within the visible angle span given in d
+          if (e.x >= d.x && e.x < (d.x + d.dx)) {
+            // get a selection of the associated text element
+            var arcText = d3.select(this.parentNode).select("text");
+            // fade in the text element and recalculate positions
+            arcText.transition().duration(750)
+              .attr("opacity", 1)
+              .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+              .attr("x", function(d) { return y(d.y); });
+          }
+      });
   }
 });
 
@@ -90,7 +111,7 @@ function arcTweenData(a, i) {
 }
 
 // When zooming: interpolate the scales.
-function arcTweenZoom(d) {
+function arcTween(d) {
   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
       yd = d3.interpolate(y.domain(), [d.y, 1]),
       yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
@@ -99,4 +120,9 @@ function arcTweenZoom(d) {
         ? function(t) { return arc(d); }
         : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
   };
+}
+
+
+function computeTextRotation(d) {
+  return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
 }
